613b71834c190b2b72595e2972acebf1ecc34679
Richard Torres <richardtorres314@gmail.com>
2022-02-01 12:44:45 +0100

    fixing spelling

--- pages/resources.js
+++ pages/resources.js
@@ -6,7 +6,11 @@ export default function AboutPage() {
     <Layout title="Tools & Resources">
       <Container>
         <h2>Contact us</h2>
-        <p>If you want to be part of our group, please write us by email below and tell us about you and your proposal. It would be a pleassure to meet you!</p>
+        <p>
+          If you want to be part of our group, please write us by email below
+          and tell us about you and your proposal. It would be a pleasure to
+          meet you!
+        </p>
       </Container>
       <Container>
         <h2>Short & Long Research Stays</h2>
