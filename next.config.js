37807b4bdd6e05dc5b63ebc5bdf3c963415e42cb
Richard Torres <richardtorres314@gmail.com>
2023-01-24 22:23:37 +0100

    new version

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      // "/": { page: "/" },
+      // "/education": { page: "/education" },
+      // "/research": { page: "/research" },
+      // "/resources": { page: "/resources" },
+      // "/talks": { page: "/talks" },
+      // "/team": { page: "/team" },
+      ...defaultPathMap,
+    };
+  },
 };
